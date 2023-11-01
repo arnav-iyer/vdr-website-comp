2e39b1bc53a8d6093f4c569f374c7a538cf1e110
arnav-iyer <119526190+arnav-iyer@users.noreply.github.com>
2023-11-01 15:50:39 -0700

    Update myV0Component.js

--- pages/myV0Component.js
+++ pages/myV0Component.js
@@ -173,7 +173,7 @@ export default function Component() {
             <img
               alt="Product Image"
               className="object-cover w-full h-48"
-              height={100}
+              height={300}
               src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/pyyixbczj6u5kiwhpjik/air-max-270-womens-shoes-Pgb94t.png"
               style={{
                 aspectRatio: "200/200",
